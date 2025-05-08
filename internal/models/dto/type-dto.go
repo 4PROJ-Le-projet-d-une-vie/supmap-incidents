fa4aa7accee441beefa4139beb54762445d7bd41
Ewennn <ewen.bosquet@gmail.com>
2025-05-08 19:30:58 +0200

    feat/ Type dto returns if incident type need route recalculation

--- internal/models/dto/type-dto.go
+++ internal/models/dto/type-dto.go
@@ -3,15 +3,17 @@ package dto
 import "supmap-users/internal/models"
 
 type TypeDTO struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID                int64  `json:"id"`
+	Name              string `json:"name"`
+	Description       string `json:"description"`
+	NeedRecalculation bool   `json:"need_recalculation"`
 }
 
 func TypeToDTO(iType *models.Type) *TypeDTO {
 	return &TypeDTO{
-		ID:          iType.ID,
-		Name:        iType.Name,
-		Description: iType.Description,
+		ID:                iType.ID,
+		Name:              iType.Name,
+		Description:       iType.Description,
+		NeedRecalculation: iType.NeedRecalculation,
 	}
 }
